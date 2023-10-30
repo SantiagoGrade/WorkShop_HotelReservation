64192467e0c37458ec8eb735f753cca58561b016
SantiagoGrade <santiago54911@gmail.com>
2023-10-29 22:42:55 -0500

    Add base project

---
+++ src/main/java/com/example/HotelReservationWorkshop/HotelReservationWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.HotelReservationWorkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelReservationWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelReservationWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HotelReservationWorkshop/HotelReservationWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HotelReservationWorkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReservationWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
