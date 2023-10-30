cff5105dc1637585efd7a11adab45318bc60d05d
SantiagoGrade <santiago54911@gmail.com>
2023-10-29 23:17:01 -0500

    Add in package dto funtionality POO in the three class

--- src/main/java/com/example/HotelReservationWorkshop/dto/Client.java
+++ src/main/java/com/example/HotelReservationWorkshop/dto/Client.java
@@ -1,4 +1,76 @@
 package com.example.HotelReservationWorkshop.dto;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "client")
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long cedula;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name="email")
+    private String email;
+
+    public Client(){
+
+    }
+
+    public Client(Long cedula, String name, String lastName, String address, String email) {
+        this.cedula = cedula;
+        this.name = name;
+        this.lastName = lastName;
+        this.address = address;
+        this.email = email;
+    }
+
+    public Long getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(Long cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- src/main/java/com/example/HotelReservationWorkshop/dto/Room.java
+++ src/main/java/com/example/HotelReservationWorkshop/dto/Room.java
@@ -1,4 +1,53 @@
 package com.example.HotelReservationWorkshop.dto;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="room")
 public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "number_room")
+    private long numberRoom;
+
+    @Column(name = "type_room")
+    private String typeRoom;
+
+    @Column(name = "original_price")
+    private double originalPrice;
+
+    public Room() {
+    }
+
+    public Room(long numberRoom, String typeRoom, double originalPrice) {
+        this.numberRoom = numberRoom;
+        this.typeRoom = typeRoom;
+        this.originalPrice = originalPrice;
+    }
+
+    public long getNumberRoom() {
+        return numberRoom;
+    }
+
+    public void setNumberRoom(long numberRoom) {
+        this.numberRoom = numberRoom;
+    }
+
+    public String getTypeRoom() {
+        return typeRoom;
+    }
+
+    public void setTypeRoom(String typeRoom) {
+        this.typeRoom = typeRoom;
+    }
+
+    public double getOriginalPrice() {
+        return originalPrice;
+    }
+
+    public void setOriginalPrice(double originalPrice) {
+        this.originalPrice = originalPrice;
+    }
 }
