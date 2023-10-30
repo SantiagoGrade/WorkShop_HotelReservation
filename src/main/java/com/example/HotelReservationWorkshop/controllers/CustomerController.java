160af3de562d9f09de62f52424d6d7ca0159d9c7
SantiagoGrade <santiago54911@gmail.com>
2023-10-30 01:46:09 -0500

    Add functions

--- src/main/java/com/example/HotelReservationWorkshop/controllers/CustomerController.java
+++ src/main/java/com/example/HotelReservationWorkshop/controllers/CustomerController.java
@@ -1,4 +1,35 @@
 package com.example.HotelReservationWorkshop.controllers;
 
+
+import com.example.HotelReservationWorkshop.dto.Customer;
+import com.example.HotelReservationWorkshop.services.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customer")
 public class CustomerController {
+    private CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Customer> registerCustomer(@RequestBody Customer customer) {
+        this.customerService.registerClient(customer);
+        return  ResponseEntity.ok(customer);
+    }
+
+    @GetMapping("/show")
+    public ResponseEntity<List<Customer>> getCustomers() {
+        List<Customer> customers = this.customerService.obtainAll();
+        return new ResponseEntity<>(customers, HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/example/HotelReservationWorkshop/controllers/RoomController.java
+++ src/main/java/com/example/HotelReservationWorkshop/controllers/RoomController.java
@@ -1,4 +1,34 @@
 package com.example.HotelReservationWorkshop.controllers;
 
+import com.example.HotelReservationWorkshop.dto.Room;
+import com.example.HotelReservationWorkshop.services.RoomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/rooms")
 public class RoomController {
+    private RoomService service;
+
+    @Autowired
+    public RoomController(RoomService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerRoom() {
+        this.service.registerRoom();
+        return ResponseEntity.ok("room register success");
+    }
+
+    public ResponseEntity<List<Room>> getRooms() {
+        List<Room> rooms = this.service.getRooms();
+        return new ResponseEntity<>(rooms, HttpStatus.OK);
+    }
 }
